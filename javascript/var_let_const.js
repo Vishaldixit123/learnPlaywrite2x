06442cd5a719fedbcbeac2c4a2cdc6783d25485b
vishal.dixit <vdixit@etq.com>
2026-05-13 10:09:44 +0530

    Add function.js and var_let_const.js

---
+++ javascript/var_let_const.js
@@ -0,0 +1,38 @@
+var a = 10;
+let b = 20;
+const c = 30;
+
+console.log(a); // 10
+console.log(b); // 20
+console.log(c); // 30
+
+var frute = ['apple', 'banna', 'grips']
+console.log(frute.length)
+
+for (var i = 0; i < frute.length; i++) {
+    console.log(frute[i]);
+}
+
+console.log('Length of the frute ' + i)
+
+console.log('Hi');
+console.log('Hi');
+console.log('Hi');
+
+function say(s) {
+    console.log(s);
+}
+say('Hi')
+say('Hi')
+say('Hi')
+
+let status = 'pending'
+if (status === 'Pending') {
+    let message = "Please wait"
+}
+//console.log(message) // refrence error
+
+const baseUrl = "https:learning.com"
+baseUrl = "usrl"
+console.log(baseUrl);
+
