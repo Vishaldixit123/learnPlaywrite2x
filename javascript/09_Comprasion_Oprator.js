a8ac3e79cdf2150650830b8a951fdf1582fae141
vishal.dixit <vdixit@etq.com>
2026-05-15 11:56:41 +0530

    Commit all uncommitted files

---
+++ javascript/09_Comprasion_Oprator.js
@@ -0,0 +1,22 @@
+
+let a = 10;
+
+let b = '10';
+
+// console.log((a == b)); // loose comprasion check only value not type
+
+// console.log((a === b))// stict compration chaeck value and type
+
+// console.log(0 == "")
+// console.log(0 === "")
+
+
+// console.log(true == "2")
+// console.log(true == 2)
+// console.log(true == 1)
+
+// console.log("" == 0)
+// console.log(0 == "0")
+console.log("" == "0")
+// console.log(null == 0)
+// console.log(null == undefined)
---
+++ javascript/1_Oprerator.js
@@ -0,0 +1,46 @@
+//Arithmetic Opretor
+let a = 10;
+let b = 5;
+
+console.log("Addition " + (a + b));
+console.log("Subtraction " + (a - b));
+console.log("Multiplication " + (a * b));
+console.log("Division " + (a / b));
+console.log("Modulus " + (a % b));
+
+//Assignment Opretor
+let c = 20;
+c += 5; // c = c + 5
+console.log("Addition Assignment " + c);
+
+c -= 5; // c = c - 5
+console.log("Subtraction Assignment " + c);
+
+c *= 2; // c = c * 2
+console.log("Multiplication Assignment " + c);
+
+c /= 2; // c = c / 2
+console.log("Division Assignment " + c);
+
+c %= 3; // c = c % 3
+console.log("Modulus Assignment " + c);
+
+//Comparison Opretor
+let x = 10;
+let y = 20;
+console.log("Equal to " + (x == y));
+console.log("Not Equal to " + (x != y));
+console.log("Greater than " + (x > y));
+console.log("Less than " + (x < y));
+console.log("Greater than or equal to " + (x >= y));
+console.log("Less than or equal to " + (x <= y));
+
+//Logical Opretor
+let p = true;
+let q = false;
+
+console.log("Logical AND " + (p && q));
+console.log("Logical OR " + (p || q));
+console.log("Logical NOT " + (!p));
+
+console.log((x == y));
