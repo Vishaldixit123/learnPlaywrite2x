bd0f9e0b689bd91606e8af8f39830ee3de47ae63
vishal.dixit <vdixit@etq.com>
2026-06-11 21:13:21 +0530

    callback

--- javascript/callBack/08_CallBack_Parameter.js
+++ javascript/callBack/08_CallBack_Parameter.js
@@ -7,3 +7,18 @@ function test(a, b, sum) {
 test(5, 10, function (result) {
     console.log("Sum is: " + result);
 });
+
+
+function multi(n1, n2, devide) {
+  let dr = devide(40, 20);
+
+  setTimeout(() => {
+    console.log("this is the second message " + n1 * n2);
+    console.log(dr);
+  }, 3000);
+}
+function dive(n3, n4) {
+  return n3 / n4;
+}
+
+multi(20, 10, dive)
