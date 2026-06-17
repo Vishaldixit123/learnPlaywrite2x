3d6e5b5bb55ec94c7bff9c22e37b257a31b46dce
vishal.dixit <vdixit@etq.com>
2026-06-17 21:58:52 +0530

    Playwright_basic

---
+++ javascript/playWriteBasic/tests/example.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test'
+
+test('Title', async ({ page }) => {
+  await page.goto('https://app.thetestingacademy.com/playwright/ttacart/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('abc');
+
+
+});
