811a69f2e08ce6e525bcb1cbd41ca0084bfc84c9
vishal.dixit <vdixit@etq.com>
2026-06-30 11:42:58 +0530

    accountProgram

--- javascript/OOPS_Concept/09_Account.js
+++ javascript/OOPS_Concept/09_Account.js
@@ -1,21 +1,30 @@
 class Account {
-    #balance;
-
-    constructor(balance) {
-        this.#balance = balance;
-    }
-
+    #balance = 0;
 
     addBlance(amount) {
         if (amount > 0) {
             this.#balance += amount
         }
     }
+
+    withdrraw(amount) {
+        if (amount > 0 && this.#balance >= amount) {
+            this.#balance -= amount
+            console.log('Withdrwa amount is ' + amount);
+            console.log('Availble balance in your account ' + this.#balance);
+        } else {
+            console.log('Insuffecent balanace in your account ' + amount);
+        }
+
+
+    }
     getBalance() {
-        console.log(this.#balance);
+        console.log('Your current balance is ' + this.#balance);
     }
 }
 
-let acc1 = new Account(50000)
-acc1.addBlance(10000)
+let acc1 = new Account()
+acc1.addBlance(500000)
+acc1.getBalance();
+acc1.withdrraw(50000);
 acc1.getBalance();
